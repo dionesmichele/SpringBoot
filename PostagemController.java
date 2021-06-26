a579af5aa13d4505f6330000634481a5cc393761
Michele Diones <85254821+nobodyknony@users.noreply.github.com>
2021-06-26 00:34:23 -0300

    Add files via upload

---
+++ PostagemController.java
@@ -0,0 +1,54 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.generation.blogPessoal.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/Postagens")
+@CrossOrigin ("*")
+
+	public class PostagemController {
+
+	@Autowired
+		private PostagemRepository repository;
+	@GetMapping
+			public ResponseEntity <List<Postagem>> GetAll (){
+			 return ResponseEntity.ok(repository.findAll());
+				
+		}
+			
+
+}		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public PostagemRepository getRepository() {
+	return repository;
+}
+
+public void setRepository(PostagemRepository repository) {
+	this.repository = repository;
+}*/
